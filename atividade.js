2c668b7d90a28b0165d0ffc4e992bf58420d0b15
Diegoxinn <110376504+Diegoxinn@users.noreply.github.com>
2025-03-31 20:14:28 -0300

    atividade.js

---
+++ atividade.js
@@ -0,0 +1,9 @@
+// Alterei a cor e o tamanho do título da pagina na Wikipedia sobre o Guarujá.
+
+function corTitulo() {
+    let titulo = document.getElementById("firstHeading"); 
+        titulo.style.color = "green";
+        titulo.style.fontSize = "150px"  
+    }   
+
+corTitulo();
